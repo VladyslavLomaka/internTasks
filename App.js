3f856fb5fbcb08529e35f4ef83d47fc948f14080
VLomaka <reyxd11@gmail.com>
2023-01-17 16:14:21 +0200

    custom fonts added

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fonts] = useFonts({
+    'OpenSans-Regular': require('./assets/fonts/OpenSans/OpenSans-Regular.ttf'),
+    'OpenSans-Bold': require('./assets/fonts/OpenSans/OpenSans-Bold.ttf'),
+    'OpenSans-Italic': require('./assets/fonts/OpenSans/OpenSans-Italic.ttf'),
+  });
+
+  if (!fonts) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={{ fontFamily: 'OpenSans-Regular' }}>Hello World!</Text>
+      <Text style={{ fontFamily: 'OpenSans-Bold' }}>Hello World!</Text>
+      <Text style={{ fontFamily: 'OpenSans-Italic' }}>Hello World!</Text>
       <StatusBar style='auto' />
     </View>
   );
