297141a39105510536e041432be6e959952e7fde
VLomaka <reyxd11@gmail.com>
2023-01-17 18:00:14 +0200

    MaskedView and gradient text

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MaskedView from '@react-native-masked-view/masked-view';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <MaskedView
+      style={{ flex: 1, flexDirection: 'row' }}
+      maskElement={
+        <View style={styles.container}>
+          <Text
+            style={{
+              fontSize: 60,
+              fontWeight: 'bold',
+            }}>
+            Gradient Text
+          </Text>
+        </View>
+      }>
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#324376' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F5DD90' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F76C5E' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#e1e1e1' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#324376' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F5DD90' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F76C5E' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#e1e1e1' }} />
+    </MaskedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
   },
