68fe5c102657b2ea103cce38c5915c7c702887df
VLomaka <reyxd11@gmail.com>
2023-01-18 02:22:38 +0200

    added splash screen

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+setTimeout(() => {
+  SplashScreen.hideAsync();
+}, 5000);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={{ letterSpacing: 3, textAlign: 'center', paddingHorizontal: 20 }}>
+        actually, it was custom splash screen, by the way!
+      </Text>
+      <Text style={{ color: '#d1d2d3', letterSpacing: 2, marginTop: 20 }}>
+        relpad app to watch again
+      </Text>
       <StatusBar style='auto' />
     </View>
   );
