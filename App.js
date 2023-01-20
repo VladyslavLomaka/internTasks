3067e1a8799a74a8e220ba49fe71fdde05825f29
VLomaka <reyxd11@gmail.com>
2023-01-20 13:45:44 +0200

    expo-device

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Device from 'expo-device';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>
+        I got that your device is {Device.deviceName} by {Device.brand}
+      </Text>
       <StatusBar style='auto' />
     </View>
   );
